e786a276bba677187c80ae9775043f51d96c36b7
Jayden Windle <jaydenwindle@gmail.com>
2022-07-18 16:20:44 -0700

    Initial Commit

---
+++ docknouns/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  docknouns
+//
+//  Created by Jayden Windle on 2022-07-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ docknouns/docknounsApp.swift
@@ -0,0 +1,17 @@
+//
+//  docknounsApp.swift
+//  docknouns
+//
+//  Created by Jayden Windle on 2022-07-18.
+//
+
+import SwiftUI
+
+@main
+struct docknounsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
