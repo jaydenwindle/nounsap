69ed2de3c7d637ec8f51f339be7f78bc9e39e071
Jayden Windle <jaydenwindle@gmail.com>
2022-07-21 19:38:47 -0700

    fixed slow updates when toggling nouns modes

--- docknouns/docknounsApp.swift
+++ docknouns/docknounsApp.swift
@@ -31,7 +31,13 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
     @Published var endTime: Date!
     
     init() {
-        setNounType(newNounType: NounType.lilnouns)
+        nounType = NounType.nouns
+        auctionHouseAddress = EthereumAddress("0x830bd73e4184cef73443c15111a1df14e495c706")! // Nouns
+        nounsContractAddress = EthereumAddress("0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03")! // Nouns
+        
+        Task.detached {
+            await self.fetchCurrentAuction()
+        }
         
         socketProvider = InfuraWebsocketProvider("wss://eth-mainnet.g.alchemy.com/v2/UQ3BdPV4IAfBQ5VcNmYoHf1xBitfyerN", delegate: self)!
         socketProvider.connectSocket()
@@ -43,7 +49,7 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
                                                                   name: NSWorkspace.willSleepNotification, object: nil)
     }
     
-    @MainActor func fetchCurrentAuction() async {
+    func fetchCurrentAuction() async {
         let auctionHouseABIPath = Bundle.main.path(forResource: "auctionHouseABI", ofType: "json")
         guard let auctionHouseABI = try? String(contentsOfFile: auctionHouseABIPath!, encoding: String.Encoding.utf8) else { return }
         
@@ -61,20 +67,25 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
                 print(endTime)
                 let timestamp = Web3.Utils.formatToPrecision(endTime as! BigUInt, numberDecimals: 0)!
                 let date = Date(timeIntervalSince1970: Double(timestamp)!)
-
-                self.endTime = date
+                await MainActor.run {
+                    self.endTime = date
+                }
             }
             
             let amount = auctionResult["amount"]!
             
-            self.amount = "Ξ" + Web3.Utils.formatToEthereumUnits(amount as! BigUInt, toUnits: .eth, decimals: 2)!
+            await MainActor.run {
+                self.amount = "Ξ" + Web3.Utils.formatToEthereumUnits(amount as! BigUInt, toUnits: .eth, decimals: 2)!
+            }
             
             if let nounId = auctionResult["nounId"] {
                 let nounIdString = Web3.Utils.formatToPrecision(nounId as! BigUInt, numberDecimals: 0)!
                 if nounIdString != self.nounId {
                     await fetchNounImage(nounId: nounIdString)
                 }
-                self.nounId = nounIdString
+                await MainActor.run {
+                    self.nounId = nounIdString
+                }
             }
         } else {
             // retry
@@ -102,7 +113,7 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
         
         if let dictionary = json as? [String: Any] {
             if let image = dictionary["image"] as? String {
-                DispatchQueue.main.async {
+                await MainActor.run {
                     self.imageURI = URL(string: image)
                     NSApp.dockTile.display()
                 }
@@ -123,27 +134,25 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
         return "ended"
     }
     
-    func setNounType(newNounType: NounType) {
-        nounType = newNounType
-        
-        print(newNounType)
-        
-        if newNounType == NounType.nouns {
-            auctionHouseAddress = EthereumAddress("0x830bd73e4184cef73443c15111a1df14e495c706")! // Nouns
-            nounsContractAddress = EthereumAddress("0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03")! // Nouns
+    func setNounType(newNounType: NounType) async {
+        await MainActor.run {
+            nounType = newNounType
+            
+            if newNounType == NounType.nouns {
+                auctionHouseAddress = EthereumAddress("0x830bd73e4184cef73443c15111a1df14e495c706")! // Nouns
+                nounsContractAddress = EthereumAddress("0x9C8fF314C9Bc7F6e59A9d9225Fb22946427eDC03")! // Nouns
+            }
+            
+            if newNounType == NounType.lilnouns {
+                auctionHouseAddress = EthereumAddress("0x55e0f7a3bb39a28bd7bcc458e04b3cf00ad3219e")! // lil nouns
+                nounsContractAddress = EthereumAddress("0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B")! // lil nouns
+            }
         }
         
-        if newNounType == NounType.lilnouns {
-            auctionHouseAddress = EthereumAddress("0x55e0f7a3bb39a28bd7bcc458e04b3cf00ad3219e")! // lil nouns
-            nounsContractAddress = EthereumAddress("0x4b10701Bfd7BFEdc47d50562b76b436fbB5BdB3B")! // lil nouns
-        }
-        
-        Task {
-            await fetchCurrentAuction()
-            socketProvider.disconnectSocket()
-            socketProvider.connectSocket()
-            try! (socketProvider).subscribeOnLogs(addresses: [auctionHouseAddress], topics: ["0x1159164c56f277e6fc99c11731bd380e0347deb969b75523398734c252706ea3", "0xc9f72b276a388619c6d185d146697036241880c36654b1a3ffdad07c24038d99"])
-        }
+        await fetchCurrentAuction()
+        socketProvider.disconnectSocket()
+        socketProvider.connectSocket()
+        try! (socketProvider).subscribeOnLogs(addresses: [auctionHouseAddress], topics: ["0x1159164c56f277e6fc99c11731bd380e0347deb969b75523398734c252706ea3", "0xc9f72b276a388619c6d185d146697036241880c36654b1a3ffdad07c24038d99"])
     }
     
     func received(message: Any) {
@@ -169,6 +178,15 @@ class NounsViewModel: ObservableObject, Web3SocketDelegate {
     @objc func onSleepNote(note _: NSNotification) {
         socketProvider.disconnectSocket()
     }
+    
+    func toggleNounsMode() async {
+        // toggle noun type
+        if (self.nounType == NounType.nouns) {
+            await self.setNounType(newNounType: NounType.lilnouns)
+        } else {
+            await self.setNounType(newNounType: NounType.nouns)
+        }
+    }
 }
 
 class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
@@ -181,23 +199,32 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
     }
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // disable main window
         if let window = NSApplication.shared.windows.first {
             window.orderOut(self)
         }
         
+        // initialize statusbar
         self.statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let statusButton = self.statusItem.button {
             statusButton.action = #selector(self.togglePopover)
             statusButton.sendAction(on: [.leftMouseUp, .rightMouseUp])
         }
         
+        // refresh statusbar title once per second (for countdown)
         let timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(refreshTitle), userInfo: nil, repeats: true)
         timer.fire()
 
+        //  initialize popover view
         popover = NSPopover()
         popover.contentSize = NSSize(width: 300, height: 300)
         popover.behavior = .transient
         popover.contentViewController = NSHostingController(rootView: ContentView(viewModel: viewModel))
+        
+        // initialize dock icon view
+        let dockIconView = DockIconView(viewModel: viewModel)
+        NSApp.dockTile.contentView = NSHostingView(rootView: dockIconView)
+        NSApp.dockTile.display()
     }
     
     @objc func refreshTitle() {
@@ -218,12 +245,10 @@ class AppDelegate: NSObject, NSApplicationDelegate, ObservableObject {
         print(NSEvent.modifierFlags.contains(.option))
 
         if NSEvent.modifierFlags.contains(.option) {
-            // toggle noun type
-            if (viewModel.nounType == NounType.nouns) {
-                viewModel.setNounType(newNounType: NounType.lilnouns)
-            } else {
-                viewModel.setNounType(newNounType: NounType.nouns)
+            Task.detached {
+                await self.viewModel.toggleNounsMode()
             }
+            
             return
         }
 
